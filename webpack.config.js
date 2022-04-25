0a1883c663d503cb47fca272ae63f658984f5bb7
rfdo93 <draox17@gmail.com>
2022-04-24 20:05:17 -0400

    Configurar url loader para gestionar fonts

--- webpack.config.js
+++ webpack.config.js
@@ -3,11 +3,13 @@ const HtmlWebpackPlugin = require('html-webpack-plugin')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
 const CopyPlugin = require('copy-webpack-plugin')
 
+
 module.exports = {
     entry: './src/index.js',
     output: {
         path:path.resolve(__dirname, 'dist'),
         filename:'main.js',
+        assetModuleFilename: 'assets/images/[hash][ext][query]'
     },
     resolve: {
         extensions: ['.js']
@@ -36,6 +38,20 @@ module.exports = {
             {
                 test:/\.png/,
                 type: 'asset/resource'
+            },
+            {
+                test:/\.(woff|woff2)$/,
+                use: {
+                    loader: 'url-loader',
+                    options: {
+                        limit: 10000,
+                        mimetype: "application/font-woff",
+                        name: "[name].[ext]",
+                        outputPath: "./assets/fonts/",
+                        publicPath: "./assets/fonts/",
+                        esModule:false
+                    }
+                }
             }
         ]
     },
